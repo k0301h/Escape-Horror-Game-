c59d01afbfa963772539756801bb6da5e92f3002
inhogo(2019180003) <dlsghdmsqlc@naver.com>
2025-01-29 01:48:44 +0900

    Player Sound Add(Foot, Breath)

---
+++ Assets/03.Scripts/Player/PlayerSoundPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerSoundPlayer : MonoBehaviour
+{
+    [SerializeField] private AudioSource rightFootSound;
+    [SerializeField] private AudioSource leftFootSound;
+    [SerializeField] private AudioSource BreathSound;
+
+    public void PlaySound(string soundName)
+    {
+        if (soundName == "rightFoot")
+        {
+            rightFootSound.Play();
+        }
+        else if (soundName == "leftFoot")
+        {
+            leftFootSound.Play();
+        }
+        else if (soundName == "Breath")
+        {
+            BreathSound.Play();
+        }
+    }
+}
