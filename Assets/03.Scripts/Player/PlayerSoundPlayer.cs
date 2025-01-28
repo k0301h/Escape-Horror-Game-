8631f88a80dfb7de3a4c8cc7e45aab4fa2bf14b3
inhogo(2019180003) <dlsghdmsqlc@naver.com>
2025-01-29 02:05:55 +0900

    Breath Sound Smootly Patch

--- Assets/03.Scripts/Player/PlayerSoundPlayer.cs
+++ Assets/03.Scripts/Player/PlayerSoundPlayer.cs
@@ -1,11 +1,29 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class PlayerSoundPlayer : MonoBehaviour
 {
     [SerializeField] private AudioSource rightFootSound;
     [SerializeField] private AudioSource leftFootSound;
-    [SerializeField] private AudioSource BreathSound;
+    [SerializeField] private AudioSource inhaleBreathSound;
+    [SerializeField] private AudioSource exhaleBreathSound;
 
+    void Start()
+    {
+        StartCoroutine(BreathCorutine());
+    }
+
+    IEnumerator BreathCorutine()
+    {
+        while (true)
+        {
+            inhaleBreathSound.Play();
+            yield return new WaitForSeconds(1.0f);
+            exhaleBreathSound.Play();
+            yield return new WaitForSeconds(1.5f);
+        }
+    }
+    
     public void PlaySound(string soundName)
     {
         if (soundName == "rightFoot")
@@ -16,9 +34,13 @@ public class PlayerSoundPlayer : MonoBehaviour
         {
             leftFootSound.Play();
         }
-        else if (soundName == "Breath")
+        else if (soundName == "inhaleBreath")
         {
-            BreathSound.Play();
+            inhaleBreathSound.Play();
+        }
+        else if (soundName == "exhaleBreath")
+        {
+            exhaleBreathSound.Play();
         }
     }
 }
