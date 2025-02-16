3b59fe44a05d66a0146aae8bd6b93f508d97ca80
inhogo(2019180003) <dlsghdmsqlc@naver.com>
2025-02-17 01:46:44 +0900

    Elevator Manequin Animation Error Fixed

--- Assets/03.Scripts/Event/UnitSpawnEvent.cs
+++ Assets/03.Scripts/Event/UnitSpawnEvent.cs
@@ -5,12 +5,26 @@ public class UnitSpawnEvent : MonoBehaviour
     [SerializeField] private GameObject unitGameObject;
     [SerializeField] private Transform unitParentTransform;
     [SerializeField] private Transform unitPosition;
+    private bool _isAnimation = false;
+
+    public void SetAnimation(bool isAnimation)
+    {
+        _isAnimation = isAnimation;
+    }
     
     public void SpawnUnit()
     {
         GameObject instance = Instantiate(unitGameObject, unitParentTransform);
         instance.transform.position = unitPosition.position;
         instance.transform.rotation = unitPosition.rotation;
+
+        if (_isAnimation)
+        {
+            if (instance.TryGetComponent(out Manequin manequin))
+            {
+                manequin.PlayRotation();
+            }
+        }
     }
 
     public void DestroyUnitToString(string unitName)
