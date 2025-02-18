b535c778bfca89ad330c924617bb94c41b14b076
inhogo(2019180003) <dlsghdmsqlc@naver.com>
2025-02-19 01:10:58 +0900

    Zombie Spawn, Chase Move Develop

--- Assets/03.Scripts/Creature/Zombie.cs
+++ Assets/03.Scripts/Creature/Zombie.cs
@@ -1,9 +1,44 @@
+using System.Collections;
 using UnityEngine;
 
 public class Zombie : Creature
 {
+    [SerializeField] private GameObject player;
+    [SerializeField] private CharacterController cc;
+    [SerializeField] private float moveSpeed = 3f;
+    
+    private bool _isRunning;
+    
     private static readonly int SeatIdle = Animator.StringToHash("tSeatIdle");
     private static readonly int SeatClapping = Animator.StringToHash("tSeatClapping");
+    private static readonly int Run = Animator.StringToHash("tRun");
+
+    #region Find Destination System
+
+    void Start()
+    {
+        _isRunning = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (_isRunning)
+        {
+            transform.LookAt(player.transform);
+
+            var direction = (player.transform.position - transform.position).normalized;
+            cc.Move(moveSpeed * TimeManager.Instance.DeltaTime() * direction);
+        }
+    }
+
+    public void StartChase()
+    {
+        _isRunning = true;
+    }
+
+    #endregion
+    
+    #region Animation
 
     public void Play_SeatIdle()
     {
@@ -14,4 +49,12 @@ public class Zombie : Creature
     {
         animator.SetTrigger(SeatClapping);
     }
+
+    public void Play_Run()
+    {
+        animator.SetTrigger(Run);
+    }
+
+    #endregion
+
 }
--- Assets/03.Scripts/Player/PlayerController.cs
+++ Assets/03.Scripts/Player/PlayerController.cs
@@ -173,7 +173,7 @@ public class PlayerController : MonoBehaviour
 
             #endregion
 
-            _cc.Move(_moveSpeed * (TimeManager.Instance == null ? Time.deltaTime : TimeManager.Instance.DeltaTime()) *
+            _cc.Move(_moveSpeed * TimeManager.Instance.DeltaTime() *
                      moveDirection);
 
             if (Input.GetKeyUp(KeyCode.W))
